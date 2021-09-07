e2e2c16bf6cc9af05e57e686d17a1cc1e84d9916
Lizbeth Baez <lizbaez140799@gmail.com>
2021-09-07 14:46:57 -0700

    7sept

---
+++ index.js
@@ -0,0 +1,9 @@
+import PKG from './package.json';
+import Block from './src/blockchain/block';
+
+const { name, version , description, author } = PKG;
+
+console.log(`${name} ${version} ${description} ${author}`);
+
+const block = new Block(Date.now(), 'pr3v10sh4sh', 'h4sh', 'd4t4');
+console.log(block.toString());
