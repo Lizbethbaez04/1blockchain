5879c29e2a843b74f7ced707414581795862645c
Lizbeth Baez <lizbaez140799@gmail.com>
2021-09-21 14:01:06 -0700

    mine

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const { name, version , description, author } = PKG;
 console.log(`${name} ${version} ${description} ${author}`);
 
 const { genesis } = Block;
-console.log(genesis.toString());
 
-const block = new Block(Date.now(), genesis.hash, 'h4sh', 'd4t4');
-console.log(block.toString());
+const block1 = Block.mine(genesis, 'transact1');
+console.log(block1.toString());
 
-const block2 = new Block(Date.now(), block.hash, 'h4sh-block2', 'd4t4-2');
+const block2 = Block.mine(block1, 'transact2');
 console.log(block2.toString());
--- src/blockchain/block.js
+++ src/blockchain/block.js
@@ -1,3 +1,4 @@
+import { SHA256 } from 'crypto-js';
 class Block 
 {
     constructor(timestamp, previousHash, hash, data)
@@ -10,9 +11,23 @@ class Block
 
     static get genesis(){
         const timestamp = (new Date(2019, 0, 1)). getTime();
-        return new this(timestamp, undefined, 'g3n3sis-h4sh', 'g3n3s1s-d4t4');
+        return new this(timestamp, undefined, 'g3n3sis-h4sh', 'transaccion0');
     }
     
+    static mine(previousBlock, data)
+    {
+        const timestamp = Date.now();
+        const { hash: previousHash } = previousBlock;
+        const hash = Block.hash(timestamp,previousHash,data);        
+
+        return new this(timestamp, previousHash, hash, data);
+    }
+
+    static hash(timestamp, previousHash, data)
+    {
+        return SHA256(`${timestamp}${previousHash},${data}`).toString();
+    }
+
     toString()
     {
         const 
